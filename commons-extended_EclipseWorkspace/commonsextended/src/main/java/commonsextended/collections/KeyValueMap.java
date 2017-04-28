57cfd83577025a9cc194b1b6914d363b16eed586
a07752a <a07752a@USALPTSGDP3.ena.us.experian.local>
2017-04-28 19:05:48 +0530

    Initial Commit

---
+++ commons-extended_EclipseWorkspace/commonsextended/src/main/java/commonsextended/collections/KeyValueMap.java
@@ -0,0 +1,12 @@
+package commonsextended.collections;
+
+import java.util.HashMap;
+
+public class KeyValueMap<K,V> extends HashMap<K,V> {
+	public KeyValueMap() {
+		super();
+	}
+	public Object get(String key) {
+		return super.get(key);
+	}
+}
